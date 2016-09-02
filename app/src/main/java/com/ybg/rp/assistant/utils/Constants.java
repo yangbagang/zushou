178844a225db72f5ed158868e9b20c97f9b033b5
yangbagang <81667842@qq.com>
2016-09-02 16:23:36 +0800

    change logo

--- app/src/main/java/com/ybg/rp/assistant/utils/Constants.java
+++ app/src/main/java/com/ybg/rp/assistant/utils/Constants.java
@@ -5,8 +5,8 @@ package com.ybg.rp.assistant.utils;
  */
 public class Constants {
 
-    //public static String HOST = "http://192.168.12.102:8080/";
+    public static String HOST = "http://192.168.12.102:8080/ua/";
 
-    public static String HOST = "http://183.57.41.230/ua/";
+    //public static String HOST = "http://183.57.41.230/ua/";
 
 }
